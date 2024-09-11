b0bd27c6f4a54dc93b581d8cfa418bee4b91109a
Aedan Pettit <apettit@goguardian.com>
2024-09-11 11:59:39 -0400

    Add routing

--- flight-booker/src/app/app.component.ts
+++ flight-booker/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor } from '@angular/common';
-import * as flightData from './flightData.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Flight Booker';
-  cities = flightData.cities;
 }
--- flight-booker/src/app/app.routes.ts
+++ flight-booker/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightSelectorComponent } from './flight-selector/flight-selector.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: FlightSearchComponent
+    },
+    {
+        path: 'select',
+        component: FlightSelectorComponent
+    }
+];
