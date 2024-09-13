894ccdb8ff7e600539dd79188a5bbeefa1b87cdf
Aedan Pettit <apettit@goguardian.com>
2024-09-13 12:43:08 -0400

    Add sort and filter

--- flight-booker/src/app/flight-selector/flight-selector.component.ts
+++ flight-booker/src/app/flight-selector/flight-selector.component.ts
@@ -9,25 +9,37 @@ import { NgFor } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Booking } from '../booking';
+// import { Booking } from '../booking';
 import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-flight-selector',
   standalone: true,
-  imports: [MatExpansionModule, MatCardModule, MatGridListModule, MatIconModule, MatButtonModule, NgFor, RouterLink],
+  imports: [MatExpansionModule, MatCardModule, MatGridListModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatSliderModule, FormsModule, ReactiveFormsModule, MatExpansionModule, NgFor, RouterLink],
   templateUrl: './flight-selector.component.html',
   styleUrl: './flight-selector.component.css'
 })
 export class FlightSelectorComponent {
 
   search: FlightSearch = {origin: "", destination: "", departureDate: ""};
-  booking: Booking = {flightId: "", firstName: "", lastName: "", email: "", bookingCode: "", fareClass: ""};
+ // booking: Booking = {flightId: "", firstName: "", lastName: "", email: "", bookingCode: "", fareClass: ""};
   availableFlights: Flight[] = [];
+  availableAirlines: string[] = [];
+  minPrice: number = 0;
+  maxPrice: number = 0;
+  selectedMinPrice: number = 0;
+  selectedMaxPrice: number = 0;
+  chosenAirlines = new FormControl([]);
+  sortOption = new FormControl('');
+
   constructor(private appService:AppService) {
     this.appService.getSearch.subscribe(s => this.search = s);
-    this.appService.getBooking.subscribe(b => this.booking = b);
+   // this.appService.getBooking.subscribe(b => this.booking = b);
   }
 
   convertTime(time: string): string {
@@ -43,8 +55,8 @@ export class FlightSelectorComponent {
     return `${hours}hr ${minutes}m`;
   }
 
-  selectFlight(id: string, fareClass: string){
-    this.appService.setBooking({
+ selectFlight(id: string, fareClass: string){
+    /*this.appService.setBooking({
       flightId: id, 
       firstName: "", 
       lastName: "", 
@@ -53,13 +65,12 @@ export class FlightSelectorComponent {
       fareClass: fareClass
     });
 
-    console.log("Booking: ", this.booking);
+    console.log("Booking: ", this.booking);*/
   }
 
-  ngOnInit(): void {
-    const flightOptions: Flight[] = flightData.flights;
+  initialFilter(): Flight[] {
     const dayOfWeek = new Date(this.search.departureDate).toLocaleString('en-us', {weekday: 'long'});
-    this.availableFlights = flightOptions.filter((flight) => {
+    return flightData.flights.filter((flight) => {
       if (flight.origin !== this.search.origin) {
         return false;
       }
@@ -73,6 +84,102 @@ export class FlightSelectorComponent {
         return true;
       }
     });
+  }
+
+  getPriceRange(): number[] {
+    const prices: number[] = [];
+    this.availableFlights.forEach(flight => {
+      prices.push(flight.economyPrice, flight.businessPrice, flight.firstPrice);
+    });
+    return [Math.min(...prices), Math.max(...prices)];
+  }
+
+  ngOnInit(): void {
+    this.availableFlights = this.initialFilter();
+    this.availableAirlines = this.availableFlights.map(flight => flight.airline);
+    const priceRange = this.getPriceRange();
+    this.minPrice = priceRange[0];
+    this.maxPrice = priceRange[1];
+    this.selectedMinPrice = this.minPrice;
+    this.selectedMaxPrice = this.maxPrice;
+  }
+
+  filterByAirline() {
+    const selectedAirlines: string[] = this.chosenAirlines.value ? this.chosenAirlines.value : [];
+    if (selectedAirlines.length === 0) {
+      this.availableFlights = this.initialFilter();
+      return;
+    }
+
+    this.availableFlights = this.initialFilter().filter((flight) => {
+      if (selectedAirlines?.includes(flight.airline)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+    const priceRange = this.getPriceRange();
+    this.minPrice = priceRange[0];
+    this.maxPrice = priceRange[1];
+  }
+
+  filterByPrice() {
+    this.filterByAirline();
+    this.availableFlights = this.availableFlights.filter((flight) => {
+      if ((flight.economyPrice >= this.selectedMinPrice
+            || flight.businessPrice >= this.selectedMinPrice
+            || flight.firstPrice >= this.selectedMinPrice)
+          && (flight.firstPrice <= this.selectedMaxPrice
+            || flight.businessPrice <= this.selectedMaxPrice
+            || flight.economyPrice <= this.selectedMaxPrice)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+  }
+
+  sortFlights() {
+    switch (this.sortOption.value) {
+      case "price": 
+        this.availableFlights.sort(this.sortByPrice);
+        break;
+      case "duration":
+        this.availableFlights.sort(this.sortByDuration);
+        break;
+      case "departureTime":
+        this.availableFlights.sort(this.sortByDepartureTime);
+        break;
+    }
+  } 
+
+  sortByPrice(a: Flight, b: Flight): number {
+    if (a.economyPrice < b.economyPrice) {
+      return -1;
+    } else if (a.economyPrice > b.economyPrice) {
+      return 1;
+    }
+    return 0;
+  }
+
+  sortByDuration(a: Flight, b: Flight): number {
+    if (a.duration < b.duration) {
+      return -1;
+    } else if (a.duration > b.duration) {
+      return 1;
+    }
+    return 0;
+  }
+
+  sortByDepartureTime(a: Flight, b: Flight): number {
+    if (a.departureTime < b.departureTime) {
+      return -1;
+    } else if (a.departureTime > b.departureTime) {
+      return 1;
+    }
+    return 0;
   }
 
 
