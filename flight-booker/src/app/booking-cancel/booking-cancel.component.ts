8458fbf919a70cc5d064e9fa346c1bd25b03f347
Michael Daly <mdaly19@C02Z21QXLVDN-M.local>
2024-09-16 20:56:51 -0400

    added new files

---
+++ flight-booker/src/app/booking-cancel/booking-cancel.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { Flight } from '../flight';
+import { Booking } from '../booking';
+import { AppService } from '../app.service';
+import { MatButtonModule } from '@angular/material/button';
+import * as utils from '../utils';
+import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-booking-cancel',
+  standalone: true,
+  imports: [MatButtonModule, RouterLink, MatIcon],
+  templateUrl: './booking-cancel.component.html',
+  styleUrl: './booking-cancel.component.css'
+})
+export class BookingCancelComponent {
+  booking: Booking = {flightId: "", firstName: "", lastName: "", email: "", bookingCode: "", fareClass: ""};
+  flightData: any = {};
+  bookedFlight: Flight = {id: "", date: "", origin: "", originCode: "", destination: "", destinationCode: "", departureTime: "", arrivalTime: "", duration: 0, airline: "", economyPrice: 0, businessPrice: 0, firstPrice: 0};
+  farePaid: number = 0;
+  dates: { [key: string]: string } = {}
+  convertTime = utils.convertTime;
+
+  constructor(private appService: AppService) {
+    this.appService.getBooking.subscribe(b => this.booking = b);
+    this.appService.getFlightData.subscribe(d => this.flightData = d);
+  }
+
+  cancelBooking(): void {
+    for (let i=0;i<this.flightData.bookings.length;i++) {
+      if (this.flightData.bookings[i].bookingCode == this.booking.bookingCode) {
+        this.flightData.bookings.splice(i,1)
+      }
+    }
+    this.appService.setFlightData(this.flightData);
+  }
+
+  ngOnInit(): void {
+    let date = new Date()
+    for (let i=0;i<7;i++) {
+      this.dates[date.toLocaleDateString('en-us', {weekday: 'long'})] = date.toLocaleDateString('en-us', {weekday: "short", month: "numeric", day: "numeric"})
+      date.setDate(date.getDate() + 1);
+    }
+    this.bookedFlight = this.flightData.flights.find((f: Flight) => f.id === this.booking.flightId) || this.bookedFlight;
+    switch (this.booking.fareClass) {
+      case "Economy":
+        this.farePaid = this.bookedFlight.economyPrice;
+        break;
+      case "Business":
+        this.farePaid = this.bookedFlight.businessPrice;
+        break;
+      case "First":
+        this.farePaid = this.bookedFlight.firstPrice;
+        break;
+      default:
+        this.farePaid = 0;
+    }
+  }
+}
---
+++ flight-booker/src/app/booking-viewer/booking-viewer.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppService } from '../app.service';
+import { Booking } from '../booking';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-booking-viewer',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, RouterLink, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './booking-viewer.component.html',
+  styleUrl: './booking-viewer.component.css'
+})
+export class BookingViewerComponent {
+  booking: Booking = {flightId: "", firstName: "", lastName: "", email: "", bookingCode: "", fareClass: ""};
+  flightData: any = {}
+  name: string = ''
+  code: string = ''
+  bookingList: Booking [] = []
+
+  constructor(private appService:AppService) {
+    this.appService.getBooking.subscribe(b => this.booking = b);
+    this.appService.getFlightData.subscribe(d => this.flightData = d);
+  }
+
+  isValid(): boolean {
+    return this.bookingList.filter((booking: Booking) => this.name == booking.lastName && this.code == booking.bookingCode).length == 0
+  }
+
+  selectBooking(): void {
+    let b = this.bookingList.filter((booking: Booking) => this.name == booking.lastName && this.code == booking.bookingCode)
+    this.appService.setBooking(b[0]);
+  }
+  
+  ngOnInit(): void {
+    for (let b of this.flightData.bookings) {
+      this.bookingList.push(b)
+    }
+  }
+}
