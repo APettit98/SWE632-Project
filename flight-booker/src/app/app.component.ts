6e661965e34406c4df983072f366f2fe7bfac220
Michael Daly <mdaly19@C02Z21QXLVDN-M.local>
2024-09-30 14:58:02 -0400

    Removed redundant code

--- flight-booker/src/app/app.component.ts
+++ flight-booker/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import { AppService } from './app.service';
-import { FlightSearch } from './flightSearch';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +11,5 @@ import { Router } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  search: FlightSearch = {origin: "", destination: "", departureDate: ""};
-
-  constructor(private appService:AppService, private router: Router) {
-    this.appService.setSearch(this.search);
-  }
+  
 }
