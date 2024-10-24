4af1b0a30320b76e90b35f500e55c4d7ebd826ed
Aedan Pettit <apettit@goguardian.com>
2024-10-24 14:55:15 -0400

    Adjust filter display and add to search screen

--- flight-booker/src/app/flight-search/flight-search.component.ts
+++ flight-booker/src/app/flight-search/flight-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppService } from '../app.service';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
@@ -8,18 +8,24 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlightSearch } from '../flightSearch';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-flight-search',
   standalone: true,
-  imports: [MatButtonModule, RouterLink, NgFor, CommonModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule],
+  imports: [MatButtonModule, RouterLink, NgFor, CommonModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule, MatSliderModule, MatCheckboxModule, MatGridListModule],
   templateUrl: './flight-search.component.html',
-  styleUrl: './flight-search.component.css'
+  styleUrl: './flight-search.component.css',
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class FlightSearchComponent {
 
-  flightData: any = {}
-  dates: String [] = []
+  flightData: any = {};
+  dates: String [] = [];
+  filter: any = {filterEconomy: true, filterBusiness: true, filterFirst: true, maxPrice: 2000, selectedAirlines: []};
+  sortOption: any = {};
   originFormControl = new FormControl('', Validators.required);
   destinationFormControl = new FormControl('', Validators.required);
   dateFormControl = new FormControl('', Validators.required);
@@ -29,6 +35,16 @@ export class FlightSearchComponent {
   constructor(private appService:AppService) {
     this.appService.getSearch.subscribe(s => this.search = s);
     this.appService.getFlightData.subscribe(d => this.flightData = d);
+    this.appService.getFilter.subscribe(f => this.filter = f);
+    this.appService.getSortOption.subscribe(s => this.sortOption = s);
+  }
+
+  onSelect(): void {
+    console.log(this.filter.selectedAirlines);
+    console.log(this.filter.selectedAirlines.length);
+    console.log(!this.filter.selectedAirlines.includes('United') 
+    && !this.filter.selectedAirlines.includes('Delta')
+    && !this.filter.selectedAirlines.includes('American'));
   }
 
   ngOnInit(): void {
@@ -43,6 +59,8 @@ export class FlightSearchComponent {
 
   updateSearch() {
    this.appService.setSearch(this.search);
+   this.appService.setFilter(this.filter);
+   this.appService.setSortOption(this.sortOption);
   }
 
 }
